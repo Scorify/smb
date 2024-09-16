d75129c78b54cf3c57cb85f92d993b00754767c3
1nv8rZim <msf9542@rit.edu>
2024-09-16 19:59:36 -0400

    Add Schema

--- main.go
+++ main.go
@@ -6,8 +6,21 @@ import (
 )
 
 type Schema struct {
-	Target string `json:"target"`
-	Port   int    `json:"port"`
+	Target         string `json:"target"`
+	Port           int    `json:"port"`
+	Username       string `json:"username"`
+	Password       string `json:"password"`
+	Domain         string `json:"domain"`
+	Share          string `json:"share"`
+	File           string `json:"file"`
+	Exists         bool   `json:"exists"`
+	ExactMatch     bool   `json:"match"`
+	SubstringMatch bool   `json:"substring_match"`
+	RegexMatch     bool   `json:"regex_match"`
+	SHA256         bool   `json:"sha256"`
+	MD5            bool   `json:"md5"`
+	SHA1           bool   `json:"sha1"`
+	ExpectedOutput string `json:"expected_output"`
 }
 
 func Run(ctx context.Context, config string) error {
