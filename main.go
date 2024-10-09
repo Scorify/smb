c09ca8f1815da4f501fe5b93d4ea12a7ae373042
1nv8rZim <msf9542@rit.edu>
2024-10-08 22:00:02 -0400

    Update `Run` function

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
-	"encoding/json"
 	"fmt"
 	"io"
 	"net"
@@ -77,19 +76,14 @@ func clean(input error) error {
 }
 
 func Run(ctx context.Context, config string) error {
-	schema := Schema{}
+	conf := Schema{}
 
-	err := json.Unmarshal([]byte(config), &schema)
+	err := schema.Unmarshal([]byte(config), &conf)
 	if err != nil {
 		return err
 	}
 
-	err = ValidateConfig(&schema)
-	if err != nil {
-		return err
-	}
-
-	connStr := fmt.Sprintf("%s:%d", schema.Target, schema.Port)
+	connStr := fmt.Sprintf("%s:%d", conf.Server, conf.Port)
 	conn, err := net.Dial("tcp", connStr)
 	if err != nil {
 		return fmt.Errorf("failed to connect to %s: %w", connStr, clean(err))
@@ -98,9 +92,9 @@ func Run(ctx context.Context, config string) error {
 
 	smbDialer := &smb2.Dialer{
 		Initiator: &smb2.NTLMInitiator{
-			User:     schema.Username,
-			Password: schema.Password,
-			Domain:   schema.Domain,
+			User:     conf.Username,
+			Password: conf.Password,
+			Domain:   conf.Domain,
 		},
 	}
 
@@ -110,64 +104,66 @@ func Run(ctx context.Context, config string) error {
 	}
 	defer smbConn.Logoff()
 
-	sharePath := fmt.Sprintf(`\\%s\%s`, schema.Target, schema.Share)
+	sharePath := fmt.Sprintf(`\\%s\%s`, conf.Server, conf.Share)
 	fs, err := smbConn.Mount(sharePath)
 	if err != nil {
 		return fmt.Errorf("failed to mount %s: %w", sharePath, clean(err))
 	}
 	defer fs.Umount()
 
-	file, err := fs.Open(schema.File)
+	file, err := fs.Open(conf.File)
 	if err != nil {
-		return fmt.Errorf("failed to open %s: %w", schema.File, clean(err))
+		return fmt.Errorf("failed to open %s: %w", conf.File, clean(err))
 	}
 	defer file.Close()
 
 	_, err = file.Seek(0, io.SeekStart)
 	if err != nil {
-		return fmt.Errorf("failed to seek to start of %s: %w", schema.File, clean(err))
+		return fmt.Errorf("failed to seek to start of %s: %w", conf.File, clean(err))
 	}
 
 	bodyBytes, err := io.ReadAll(file)
 	if err != nil {
-		return fmt.Errorf("failed to read %s: %w", schema.File, clean(err))
+		return fmt.Errorf("failed to read %s: %w", conf.File, clean(err))
 	}
 
-	switch {
-	case schema.Exists:
+	switch conf.MatchType {
+	case "exists":
 		return nil
-	case schema.ExactMatch:
-		if string(bodyBytes) != schema.ExpectedOutput {
-			return fmt.Errorf("response does not match expected output: %q", schema.ExpectedOutput)
+	case "exactMatch":
+		if string(bodyBytes) != conf.ExpectedOutput {
+			return fmt.Errorf("response does not match expected output: %q", conf.ExpectedOutput)
 		}
-	case schema.SubstringMatch:
-		if !strings.Contains(string(bodyBytes), schema.ExpectedOutput) {
-			return fmt.Errorf("response does not contain expected output: %q", schema.ExpectedOutput)
+	case "substringMatch":
+		if !strings.Contains(string(bodyBytes), conf.ExpectedOutput) {
+			return fmt.Errorf("response does not contain expected output: %q", conf.ExpectedOutput)
 		}
-	case schema.RegexMatch:
-		pattern, err := regexp.Compile(schema.ExpectedOutput)
+	case "regexMatch":
+		pattern, err := regexp.Compile(conf.ExpectedOutput)
 		if err != nil {
 			return fmt.Errorf("encountered error compiling regex pattern: %v", err)
 		}
 
 		if !pattern.Match(bodyBytes) {
-			return fmt.Errorf("response does not match regex pattern: %s", schema.ExpectedOutput)
+			return fmt.Errorf("response does not match regex pattern: %s", conf.ExpectedOutput)
 		}
-	case schema.SHA256:
+	case "sha256":
 		sha256 := fmt.Sprintf("%x", sha256.Sum256(bodyBytes))
-		if sha256 != schema.ExpectedOutput {
-			return fmt.Errorf("response does not match expected sha256: %s", schema.ExpectedOutput)
+		if sha256 != conf.ExpectedOutput {
+			return fmt.Errorf("response does not match expected sha256: %s", conf.ExpectedOutput)
 		}
-	case schema.MD5:
+	case "md5":
 		md5 := fmt.Sprintf("%x", md5.Sum(bodyBytes))
-		if md5 != schema.ExpectedOutput {
-			return fmt.Errorf("response does not match expected md5: %s", schema.ExpectedOutput)
+		if md5 != conf.ExpectedOutput {
+			return fmt.Errorf("response does not match expected md5: %s", conf.ExpectedOutput)
 		}
-	case schema.SHA1:
+	case "sha1":
 		sha1 := fmt.Sprintf("%x", sha1.Sum(bodyBytes))
-		if sha1 != schema.ExpectedOutput {
-			return fmt.Errorf("response does not match expected sha1: %s", schema.ExpectedOutput)
+		if sha1 != conf.ExpectedOutput {
+			return fmt.Errorf("response does not match expected sha1: %s", conf.ExpectedOutput)
 		}
+	default:
+		return fmt.Errorf("unknown match type: %s", conf.MatchType)
 	}
 
 	return nil
